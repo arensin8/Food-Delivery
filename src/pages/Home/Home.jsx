e59dd8c6b65957cebb528c90fa7e0aa1f21f5920
Aren Sinanian <95931889+arensin8@users.noreply.github.com>
2024-07-17 20:41:33 +0000

    Header section

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../../componenets/Header/Header";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div >
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,7 @@
+import "./PlaceOrder.css";
+
+const PlaceOrder = () => {
+  return <div className="PlaceOrder"></div>;
+};
+
+export default PlaceOrder;

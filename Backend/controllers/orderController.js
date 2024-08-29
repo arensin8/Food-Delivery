e9b040112619e1889db7b3513f248d069b366245
arensin8 <arensinani4@gmail.com>
2024-08-29 11:30:45 +0400

    placing order configs for back end

---
+++ Backend/controllers/orderController.js
@@ -0,0 +1,8 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import stripe from "stripe";
+
+// Placing user order for front end
+const placeOrder = async (req, res, next) => {};
+
+export { placeOrder };
